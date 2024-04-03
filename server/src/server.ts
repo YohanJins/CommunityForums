b34f2f62f83532094021f2c107fb324f454bfcf0
Yohan <yohanjin0925@gmail.com>
2024-04-03 10:11:35 -0400

    Community Forum Start. Structure with postgres in Docker

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get("/", (_, res) => res.send("running"));
+
+let port = 4000;
+app.listen(port, async () => {
+  console.log(`server runniung at http://localhost:${port}`);
+})
